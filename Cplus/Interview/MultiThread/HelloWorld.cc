9f81fd48e0b634fe694869f77a5fe8167370210f
cogitates <weifengzxq@gmail.com>
2020-04-16 20:28:12 +0800

    from deepin

--- Cplus/Interview/MultiThread/HelloWorld.cc
+++ Cplus/Interview/MultiThread/HelloWorld.cc
@@ -7,6 +7,8 @@ using namespace std;
  void call_from_thread()
  {
  	cout << "hello world!" << endl;
+	cout << "whoami" << endl;
+	cout << "abash" << endl;
  }
 //  g++ -std=c++11 HelloWorld.cc -O2 -lpthread  
 // ./a.out
@@ -18,6 +20,7 @@ using namespace std;
      cout<<"main thread"<<endl;
 	//Join the thread with the main thread
 	//和主线程协同
- 	t1.join();
+ 	t1.join(); //阻塞主线程，等待对应线程执行完毕，相当于把当前进程执行完后merge到主线程
+	// join()会在线程结束后，释放线程的资源和内存
  	return 0;
  }
